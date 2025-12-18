212aadb9326f30d3136a8ef5739675146e0214fc
JabadeSusheelKrishna <susheelkrishna95@gmail.com>
2025-12-19 00:59:01 +0530

    added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode basename="/peer-meeting-webapp-frontend">
+  <StrictMode>
     <App />
   </StrictMode>,
 )
